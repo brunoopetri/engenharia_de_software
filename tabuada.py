2c6e8f7b993ba0c86707c2771e32c0440e890e1a
Bruno Petri <98756562+brunoopetri@users.noreply.github.com>
2022-08-23 17:04:54 -0300

    Update tabuada.py

--- tabuada.py
+++ tabuada.py
@@ -1,5 +1,4 @@
 # Primeiro, perguntamos qual tabuada o usuário quer: a de 1, de 2, de 3 ou qualquer outra, e armazenamos na variável 'tabuada'.
-
 # Em seguida, usamos uma variável count que vai de 1 até 10 dentro do laço FOR e função range, veja:
 
 tabuada = int(input("Tabuada do número: "))
