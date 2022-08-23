10511650f95e5126bc9cfaa022e2f09ee458d5ec
Bruno Petri <98756562+brunoopetri@users.noreply.github.com>
2022-08-23 16:57:31 -0300

    Add files via upload

---
+++ tabuada.py
@@ -0,0 +1,8 @@
+# Primeiro, perguntamos qual tabuada o usuário quer: a de 1, de 2, de 3 ou qualquer outra, e armazenamos na variável 'tabuada'.
+
+# Em seguida, usamos uma variável count que vai de 1 até 10 dentro do laço FOR e função range, veja:
+
+tabuada = int(input("Tabuada do número: "))
+
+for count in range(10):
+    print("%d x %d = %d" % (tabuada, count+1, tabuada*(count+1)))
